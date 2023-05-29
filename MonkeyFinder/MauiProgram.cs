717fb22583710e79d05eec1b9e8d16d32bdc5356
ristokoort <114060658+ristokoort@users.noreply.github.com>
2023-05-29 17:46:49 +0300

    Add more builder.services to mauiprogram

--- MonkeyFinder/MauiProgram.cs
+++ MonkeyFinder/MauiProgram.cs
@@ -19,9 +19,16 @@ public static class MauiProgram
         builder.Logging.AddDebug();
 #endif
 
+        builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+        builder.Services.AddSingleton<IGeolocation>(Geolocation.Default);
+        builder.Services.AddSingleton<IMap>(Map.Default);
+
         builder.Services.AddSingleton<MonkeyService>();
         builder.Services.AddSingleton<MonkeysViewModel>();
         builder.Services.AddSingleton<MainPage>();
+
+        builder.Services.AddTransient<MonkeyDetailsViewModel>();
+        builder.Services.AddTransient<DetailsPage>();
 
         return builder.Build();
     }
